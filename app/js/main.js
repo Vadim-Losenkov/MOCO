d7c30881612330fa73d4cc79b660847b18123e7d
Vadim Losenkov <vprog0729@gmail.com>
2022-02-16 14:54:19 +0300

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,16 @@ $(function() {
       starWidth: "17px"
     })
   }
+  $('.open-popup').magnificPopup({
+    type: 'inline',
+    removalDelay: 500, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function () {
+        this.st.mainClass = this.st.el.attr('data-effect');
+      }
+    },
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+  });
 })
 
 
@@ -16,5 +26,13 @@ new Swiper('.team__slider', {
   navigation: {
     nextEl: '.team__arrows-right',
     prevEl: '.team__arrows-left',
+  },
+})
+new Swiper('.reviews__slider', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.reviews__arrows-right',
+    prevEl: '.reviews__arrows-left',
   },
 })
